29d194b80013d094a4a21b9bff05ff0da0970989
gennariarmando <gennariarmando@outlook.com>
2020-04-27 21:00:57 +0200

    Update Screen.h

--- Screen.h
+++ Screen.h
@@ -1,56 +1,70 @@
-#ifndef __SCREEN
-#define __SCREEN
+#ifndef AVAIL_SCREEN
+#define AVAIL_SCREEN
 
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
+#include <WTypes.h>
 
 #include <stdio.h>
 #include <cstdint>
 #include <tuple>
 #include <cmath>
 
-struct handle_data {
-    unsigned long process_id;
-    HWND window_handle;
-};
+namespace avail {
+    // Internal use only.
+    struct handle_data {
+        unsigned long process_id;
+        HWND window_handle;
+    };
 
-BOOL CALLBACK EnumWindowsCallBack(HWND handle, LPARAM lparam) {
-    auto& data = *reinterpret_cast<handle_data*>(lparam);
+    BOOL CALLBACK EnumWindowsCallBack(HWND handle, LPARAM lparam) {
+        auto& data = *reinterpret_cast<handle_data*>(lparam);
 
-    unsigned long process_id = 0;
-    GetWindowThreadProcessId(handle, &process_id);
+        unsigned long process_id = 0;
+        GetWindowThreadProcessId(handle, &process_id);
 
-    if (data.process_id != process_id) {
-        return TRUE;
+        if (data.process_id != process_id) {
+            return TRUE;
+        }
+
+        data.window_handle = handle;
+        return FALSE;
     }
 
-    data.window_handle = handle;
-    return FALSE;
-}
+    HWND FindMainWindow() {
+        handle_data data = {};
 
-HWND FindMainWindow() {
-    handle_data data = {};
+        data.process_id = GetCurrentProcessId();
+        data.window_handle = nullptr;
+        EnumWindows(EnumWindowsCallBack, reinterpret_cast<LPARAM>(&data));
 
-    data.process_id = GetCurrentProcessId();
-    data.window_handle = nullptr;
-    EnumWindows(EnumWindowsCallBack, reinterpret_cast<LPARAM>(&data));
+        return data.window_handle;
+    }
 
-    return data.window_handle;
-}
+    std::tuple<int32_t, int32_t> GetWindowRes() {
+        WINDOWINFO info = {};
+        info.cbSize = sizeof(WINDOWINFO);
+        GetWindowInfo(FindMainWindow(), &info);
+        int32_t w = info.rcClient.right - info.rcClient.left;
+        int32_t h = info.rcClient.bottom - info.rcClient.top;
+        return std::make_tuple(w, h);
+    }
 
-std::tuple<int32_t, int32_t> GetWindowRes() {
-    WINDOWINFO info = {};
-    info.cbSize = sizeof(WINDOWINFO);
-    GetWindowInfo(FindMainWindow(), &info);
-    int32_t w = info.rcClient.right - info.rcClient.left;
-    int32_t h = info.rcClient.bottom - info.rcClient.top;
-    return std::make_tuple(w, h);
-}
-
-namespace screen {
+#ifndef _WIN64
     static float fAspectRatio;
-    static float fFOV;
+    static float fWidth;
+    static float fHeight;
 
+    static float fFOV;
+    static float fFOVMult;
+#else
+    static float& fAspectRatio = Trampoline::MakeTrampoline(GetModuleHandle(nullptr))->Reference<float>();
+    static float& fWidth = Trampoline::MakeTrampoline(GetModuleHandle(nullptr))->Reference<float>();
+    static float& fHeight = Trampoline::MakeTrampoline(GetModuleHandle(nullptr))->Reference<float>();
+
+    static float& fFOV = Trampoline::MakeTrampoline(GetModuleHandle(nullptr))->Reference<float>();
+    static float& fFOVMult = Trampoline::MakeTrampoline(GetModuleHandle(nullptr))->Reference<float>();
+#endif
+
+    // Set aspect ratio based on window size.
     inline void SetAspectRatio() {
         uint32_t AspectRatioWidth, AspectRatioHeight;
         uint32_t ViewPortWidth, ViewPortHeight;
@@ -65,26 +79,25 @@ namespace screen {
         fCustomAspectRatioVer = static_cast<float>(AspectRatioHeight);
 
         fAspectRatio = fCustomAspectRatioHor / fCustomAspectRatioVer;
+        fWidth = fCustomAspectRatioHor;
+        fHeight = fCustomAspectRatioVer;
     }
 
+    // Set FOV based on original FOV/Aspect Ratio.
     inline void SetFOV(float originalFOV, float originalAR) {
         fFOV = std::round((2.0f * atan(((fAspectRatio) / (originalAR)) * tan(originalFOV / 2.0f * ((float)M_PI / 180.0f)))) * (180.0f / (float)M_PI) * 100.0f) / 100.0f;
     }
 
-    inline float GetAspectRatio() { return fAspectRatio; }
-    inline float GetFieldOfView() { return fFOV; }
+    // Set FOV based on a FOV multiplier and original Aspect Ratio.
+    inline void SetFOVMult(float originalFOVMult, float originalAR) {
+        float f = std::round((2.0f * atan(((fAspectRatio) / (originalAR)) * tan((originalFOVMult * 10000.0f) / 2.0f * ((float)M_PI / 180.0f)))) * (180.0f / (float)M_PI) * 100.0f) / 100.0f;
+        fFOVMult = f / 10000.0f;
+    }
+
+    // After initialization, call this to get respective aspect ratio and fov.
+    inline float &GetAspectRatio() { return fAspectRatio; }
+    inline float &GetFieldOfView() { return fFOV; }
+    inline float &GetFOVMult() { return fFOVMult; }
 };
-
-static void InitScreen(float originalFOV, float originalAR) {
-    float ar = screen::GetAspectRatio();
-
-    do {
-        if (ar != screen::GetAspectRatio()) {
-            screen::SetAspectRatio();
-            screen::SetFOV(originalFOV, originalAR);
-            ar = screen::GetAspectRatio();
-        }
-    } while (0);
-}
 
 #endif
